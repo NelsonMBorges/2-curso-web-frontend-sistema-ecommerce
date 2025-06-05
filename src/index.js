05db4d9e157117656f9aaa720c5dfda500e58d55
NelsonMBorges <ergonomia.life@gmail.com>
2025-06-05 14:54:25 -0300

    apagado o console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 // Lista de produtos disponíveis com nome e preço
 const produtosDisponiveis = [
   { nome: "Camisa", preco: 50.0 },
@@ -10,3 +8,26 @@ const produtosDisponiveis = [
 
 // Carrinho de compras como um array de objetos
 let carrinho = [];
+
+function adicionarProduto() {
+    let nomeProduto = prompt("Qual produto deseja adicionar? (Camisa, Calça, Sapato, Boné)").trim();
+    let produtoSelecionado = produtos.find(p => p.nome.toLowerCase() === nomeProduto.toLowerCase());
+
+    if (produtoSelecionado) {
+        let quantidade = parseInt(prompt(`Quantas unidades de ${produtoSelecionado.nome} deseja adicionar?`));
+        if (!isNaN(quantidade) && quantidade > 0) {
+            let produtoCarrinho = {
+                nome: produtoSelecionado.nome,
+                preco: produtoSelecionado.preco,
+                quantidade: quantidade,
+                subtotal: produtoSelecionado.preco * quantidade
+            };
+            carrinho.push(produtoCarrinho);
+            alert(`${quantidade}x ${produtoSelecionado.nome} adicionados ao carrinho!`);
+        } else {
+            alert("Quantidade inválida!");
+        }
+    } else {
+        alert("Produto não encontrado!");
+    }
+}
